d3866ab1b2055a3753a415d249898cf4dc3e7978
Joshua Thompson <ucfjorsh@gmail.com>
2014-08-12 21:27:16 -0700

    forgot to check in changes to genome

--- Genome.hs
+++ Genome.hs
@@ -6,6 +6,8 @@ module Genome where
 import ConnectionGene
 import NodeGene
 
+import Data.List
+
 type Genome = [ConnectionGene]
 
 addConnection genome from to weight inno = (ConnectionGene from to weight True inno):genome
@@ -27,3 +29,10 @@ createInitialGenome inputs outputs = zipWith ($) partialConstructors [0..inputs*
     where partialConstructors = [(ConnectionGene a b 1.0 True) | a <- sensorNodes, b <- outputNodes]
           sensorNodes = map (NodeGene Sensor 0.0) [0..inputs-1]
           outputNodes = map (NodeGene Output 0.0) [inputs..inputs+outputs-1]
+
+getNodeCounts genome = [sensor, hidden, output]
+    where allNodes = nub $ map from genome ++ map to genome
+          allNodeTypes = map nodeType allNodes
+          sensor = length . filter (==Sensor) $ allNodeTypes
+          hidden = length . filter (==Hidden) $ allNodeTypes
+          output = length . filter (==Output) $ allNodeTypes
